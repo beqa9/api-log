576c0284637aa45aab6fbef00fec98a061b4c0dc
beqa <beqabeqa329@gmail.com>
2025-10-17 11:11:48 +0400

    upload project swagger

---
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("event Management API")
+                        .description("This API manages events.")
+                        .version("1.0.0"));
+    }
+}
